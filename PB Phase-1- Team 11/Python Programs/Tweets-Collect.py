637e6c74c557eb23d816a37497457b870468c099
cmoulika009 <cmoulika009@gmail.com>
2016-10-05 13:53:19 -0500

    PB

---
+++ PB Phase-1- Team 11/Python Programs/Tweets-Collect.py
@@ -0,0 +1,28 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "1974127951-N1QRXNCsVCywXl67BU1Wx7VlJ1fw2TlScZDY07s"
+ACCESS_SECRET = "zLLfZ4ZF9BxvgzjXAkjJFAVFOL4P1i0fLSaZzc6LrnqZJ"
+CONSUMER_KEY = "RfqrQLUtEAvwqSmNpGjZydmOn"
+CONSUMER_SECRET = "1XdUvxMDfcL5eE89oAo7ZO8UESv6H7HacNa9B0Y7cGFa5MiPjo"
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print (data)
+        return True
+
+    def on_error(self, status):
+        print (status)
+
+if __name__ == '__main__':
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
